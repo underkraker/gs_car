b217453d258efb825eb9a259ccedcabcc8415d74
Underkraker <TU_CORREO_GITHUB>
2026-03-10 00:00:51 -0600

    Fix image upload reliability for gallery and logo

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ if (!fs.existsSync(uploadsDir)) {
 }
 
 const app = express();
-app.use(express.json());
+app.use(express.json({ limit: '15mb' }));
+app.use(express.urlencoded({ extended: true, limit: '15mb' }));
 app.use(cors());
 app.use(express.static(__dirname));
 
